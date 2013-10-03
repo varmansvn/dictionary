2ee54b2302e43cea787100fc64ee57850cd7fd46
varmansvn <varmansvn@gmail.com>
2013-10-03 19:22:56 +0700

    bootstrap the project

---
+++ app/scripts/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+angular.module('dictionaryApp', [])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,14 @@
+'use strict';
+
+angular.module('dictionaryApp')
+  .controller('MainCtrl', function ($scope) {
+
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  
+  	
+
+  });
